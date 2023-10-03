ef79e419424a6f09ecd22e4f7471ba833d207d3e
Claudia GUERRERO <117662080+Clauga@users.noreply.github.com>
2023-10-03 20:49:45 +0200

    Update Hangar.java

--- src/Hangar.java
+++ src/Hangar.java
@@ -1,7 +1,7 @@
 public class Hangar {
     public static void main(String[] args) {
-        Car car = new Car("Kia", 100000);
-        Boat boat = new Boat("Titanic", 1000000);
+        Car car = new Car("Kia", 50000);
+        Boat boat = new Boat("Zephyr", 120000);
 
         System.out.println(car.performAction());
         System.out.println(boat.performAction());
